588b2a4a6a7cf013c1369662e395d0b231c3392e
Wayne Wang <wythern@gmail.com>
2014-10-19 19:49:04 +0800

    Update findMedianNumber.
    1. Add test.
    2. Actually it returns the merged_array[(m+n-1)/2] element.
    3. Will change to official median number definition in next version.
    
    Signed-off-by: Wayne Wang <wythern@gmail.com>

--- findMedianNumber.cpp
+++ findMedianNumber.cpp
@@ -1,95 +1,120 @@
-#include <vector>
-#include <algorithm>
-#include <iostream>
+#include "common.h"
 
 using namespace std;
 
 class Solution {
-    public:
-        double findMedianSortedArrays(int A[], int m, int B[], int n) {
-            std::cout << __FUNCTION__ << std::endl;
-            for(int i = 0; i < m; ++i)
-                cout << A[i] << ",";
-            cout << endl;
-            for(int i = 0; i < n; ++i)
-                cout << B[i] << ",";
-            cout << endl;
+public:
+	double findMedianSortedArrays(int A[], int m, int B[], int n) {
+		int targetIdx = (m+n-1)/2;
+		int idxBegin = -1;
+		int idxEnd = -1;
+		std::vector<int> vA(A, A+m);
+		std::vector<int> vB(B, B+n);
+		int nLenth = n;
+		int nPos = (nLenth - 1)>>1;
 
-			std::vector<int> vA(A, A+m);
-			std::vector<int> vB(B, B+n);
-			return findMedianInVector(vA, vA.begin(), vA.end(),
-									  vB, vB.begin(), vB.end());
-        }
+		if(m == 0 && n != 0){
+			return B[(n-1)/2];
+		}else if(m != 0 && n == 0){
+			return A[(m-1)/2];
+		}
+
+		// pinpoint B
+		while((nLenth > 0) && (nPos >= 0)){
+			//using middle b to clamp a
+			int nMedianB = B[nPos];
+			idxBegin = std::distance(vA.begin(), std::lower_bound(vA.begin(), vA.end(), nMedianB)) + nPos;
+			idxEnd = std::distance(vA.begin(), std::upper_bound(vA.begin(), vA.end(), nMedianB)) + nPos;
+			//printf("B:: nLength(%d), nPos(%d), idx[%d, %d).\n", nLenth, nPos, idxBegin, idxEnd);
+
+			if(idxBegin > targetIdx && idxEnd > targetIdx){
+				nLenth = nPos;
+				nPos = (nLenth - 1)>>1;
+			}else if(idxBegin < targetIdx && idxEnd < targetIdx){
+				nLenth -= nPos + 1;
+				nPos++; // reset the start position.
+				nPos += (nLenth - 1)>>1;
+			}else{
+				return nMedianB;
+			}
+		}
+
+		// pinpoint A
+		nLenth = m;
+		nPos = (nLenth - 1)>>1;
+		while((nLenth > 0) && (nPos >= 0)){
+			//using middle b to clamp a
+			int nMedianA = A[nPos];
+			idxBegin = std::distance(vB.begin(), std::lower_bound(vB.begin(), vB.end(), nMedianA)) + nPos;
+			idxEnd = std::distance(vB.begin(), std::upper_bound(vB.begin(), vB.end(), nMedianA)) + nPos;
+			//printf("A:: nLength(%d), nPos(%d), idx[%d, %d).\n", nLenth, nPos, idxBegin, idxEnd);
+
+			if(idxBegin > targetIdx && idxEnd > targetIdx){
+				nLenth = nPos;
+				nPos = (nLenth - 1)>>1;
+			}else if(idxBegin < targetIdx && idxEnd < targetIdx){
+				nLenth -= nPos + 1;
+				nPos++; // reset the start postion.
+				nPos += (nLenth - 1)>>1;
+			}else{
+				return nMedianA;
+			}
+		}
+
+		//printf("ERROR! should not be here forever.\n");
+		return 0;
+	}
 
 private:
-    double resolveMedianNumber(std::vector<int>& vA, int nA,
-                               std::vector<int>& vB, int nB){
-        //assert(vA[nA] < vB[nB]);
-        int sA = vA.size();
-        int sB = vB.size();
-
-		if((sA + sB)%2){
-			return (nA == (sA+sB)/2 - 1) ? vA[nA] : vB[nB];
-		}else{
-			return (nA == (sA+sB)/2) ? vA[nA] : vB[nB];
-		}
-    }
-    
-    double findMedianInVector(std::vector<int>& vA,
-							  std::vector<int>::iterator iterABegin,
-							  std::vector<int>::iterator iterAEnd,
-							  std::vector<int>& vB,
-							  std::vector<int>::iterator iterBBegin,
-							  std::vector<int>::iterator iterBEnd){
-
-		std::cout << __FUNCTION__ << std::endl;
-		for(std::vector<int>::iterator iter = iterABegin; iter < iterAEnd; ++iter){
-			std::cout << *iter << ",";
-		}
-		std::cout << std::endl;
-		for(std::vector<int>::iterator iter = iterBBegin; iter < iterBEnd; ++iter){
-			std::cout << *iter << ",";
-		}
-		std::cout << std::endl;
-		
-        if (std::distance(iterABegin, iterAEnd) == 1 &&
-            std::distance(iterBBegin, iterBEnd) == 1){
-            return resolveMedianNumber(vA, std::distance(vA.begin(), iterABegin),
-                                       vB, std::distance(vB.begin(), iterBBegin));
-		}
-
-        std::vector<int>::iterator iterMidA = iterABegin + std::distance(iterABegin, iterAEnd)/2;
-        std::vector<int>::iterator iterMidB = iterBBegin + std::distance(iterBBegin, iterBEnd)/2;
-
-        if (*iterMidA == *iterMidB)
-            return *iterMidA;
-        if (*iterMidA > *iterMidB){
-            //std::swap(vA, vB);
-            //std::swap(iterMidA, iterMidB);
-			return findMedianInVector(vB, iterMidB, iterBEnd,
-									  vA, iterABegin, iterMidA);
-        }
-        // A[m/2, findinA(MidB)] and B[findinB(MidA), n/2]
-		/*
-        return findMedianInVector(vA, iterMidA, std::lower_bound(iterMidA, iterAEnd, *iterMidB),
-                                  vB, std::lower_bound(iterBBegin, iterMidB, *iterMidA), iterMidB);
-		*/
-		return findMedianInVector(vA, iterMidA, iterAEnd,
-                                  vB, iterBBegin, iterMidB);
-
-    }
 };
+
+void Test(int A[], int m, int B[], int n, int target)
+{
+   Solution s;
+   int nMedian = s.findMedianSortedArrays(A, m, B, n);
+   if(nMedian != target){
+	   printArray(A, m);
+	   printArray(B, n);
+	   printf("TEST FAILED! mid(%d) != target(%d).\n", nMedian, target);
+	   assert(0);
+   }else{
+	   printf("TEST PASSED!\n");
+   }
+}
+int A[10];
+int B[10];
 
 int main()
 {
-   cout << "Hello World" << endl; 
-   Solution s;
-   int A[] = {1, 3, 5, 7, 9};
-   int B[] = {2, 4, 6, 8, 10, 11};
-   int m = sizeof(A)/sizeof(int);
-   int n = sizeof(B)/sizeof(int);
+   cout << "Hello World" << endl;
+
+   A[0] = 2;
+   B[0] = 2;  
+   Test(A, 1, B, 0, 2);
    
-   std::cout << s.findMedianSortedArrays(A, m, B, n) << std::endl;
+   A[0] = 2;
+   B[0] = 2;  
+   Test(A, 1, B, 1, 2);
+   
+   A[0] = 1;A[1] = 2;
+   B[0] = 2;B[1] = 3;
+   Test(A, 2, B, 2, 2);
+
+   A[0] = 1;A[1] = 2;A[2] = 3;
+   B[0] = 4;B[1] = 5;
+   Test(A, 3, B, 2, 3);
+   
+   A[0] = 1;A[1] = 3;A[2] = 5;
+   B[0] = 2;B[1] = 4;
+   Test(A, 3, B, 2, 3);
+
+   A[0] = 1;A[1] = 3;A[2] = 5;
+   B[0] = 2;B[1] = 4;B[2] = 4;
+   Test(A, 3, B, 3, 3);
+
+   A[0] = 1;A[1] = 4;A[2] = 5;
+   B[0] = 2;B[1] = 4;B[2] = 4;
+   Test(A, 3, B, 3, 4);
    
    return 0;
 }

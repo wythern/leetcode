a970f149e758d7a3da12519a8b1db36e48f6f8c0
Wayne Wang <wythern@gmail.com>
2014-10-29 10:54:28 +0800

    Update PalindromeNumber to use no extra space algorithm.
    Signed-off-by: Wayne Wang <wythern@gmail.com>

--- PalindromeNumber.cpp
+++ PalindromeNumber.cpp
@@ -2,9 +2,36 @@
 
 class Solution {
 public:
-    bool isPalindrome(int x) {
+	bool isPalindrome(int x) {
 		if(x < 0)
 			return false;
+
+		int n = x;
+		int len = 0;
+		while(n){
+			len ++;
+			n /= 10;
+		}
+
+		n = x;
+		for(int i = 0; i < len; ++i){
+			int t = 0;
+			int m = x;
+			while(t++ < len - i - 1){
+				m /= 10;
+			}
+			if(n%10 != m%10)
+				return false;
+			n /= 10;
+		}
+
+		return true;
+	}
+
+    bool isPalindrome1(int x) {
+		if(x < 0)
+			return false;
+
 		int n = x;
 		const char *p = "0123456789";
 		char buffer[256];

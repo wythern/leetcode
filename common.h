78faea6970adb0d987ef96259606ec1419854558
Wayne Wang <wythern@gmail.com>
2014-11-15 21:46:21 +0800

    Fix threeSum.cpp.
    1. Use burte-force, O(n^3).
    2. optimize with map, O(n^2).
    3. optimize with hash_map, O(n^2).
    4. optimize with new algorithm, maybe O(n).
    
    Signed-off-by: Wayne Wang <wythern@gmail.com>

--- common.h
+++ common.h
@@ -5,73 +5,102 @@
 #ifndef __LEETCODE_COMMON_H__
 #define __LEETCODE_COMMON_H__
 //OS
+#include <sys/time.h>
 #include <assert.h>
 
 //C
 #include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <memory.h>
 
 //C++
 #include <climits>
 #include <vector>
 #include <set>
+#include <map>
+#include <hash_map>
 #include <algorithm>
 #include <iostream>
 #include <string>
 #include <exception>
 
+//EXT.
+#include <ext/hash_map>
+using __gnu_cxx::hash_map;
+
 struct ListNode {
-	int val;
-	ListNode *next;
+    int val;
+    ListNode *next;
 
     ListNode(int x) : val(x), next(NULL) {}
     ListNode() : val(0), next(NULL) {}
 
 public:
-	void from(int A[], int m){
-		int i = 0;
-		ListNode* p = this;
-		while(i < m){
-			if(i != 0){
-				p->next = new ListNode(A[i]);
-				p = p->next;
-			}else{
-				val = A[i];
-			}
-			i++;
-		}
-	}
+    void from(int A[], int m){
+        int i = 0;
+        ListNode* p = this;
+        while(i < m){
+            if(i != 0){
+                p->next = new ListNode(A[i]);
+                p = p->next;
+            }else{
+                val = A[i];
+            }
+            i++;
+        }
+    }
 
-	void dump(){
-		ListNode* p = this;
-		while(NULL != p){
-			std::cout << p->val << "->";
-			p = p->next;
-		}
-		std::cout << std::endl;
-	}
+    void dump(){
+        ListNode* p = this;
+        while(NULL != p){
+            std::cout << p->val << "->";
+            p = p->next;
+        }
+        std::cout << std::endl;
+    }
 };
 
 void printArray(int A[], int n, const char* pszArrayName = NULL)
 {
-	
-	if(NULL != pszArrayName){
-		printf("%s[", pszArrayName);
-	}else{
-		printf("Array[");
-	}
 
-	for(int i = 0; i < n; ++i)
-		printf("%d,", A[i]);
-	printf("]\n");
+    if(NULL != pszArrayName){
+        printf("%s[", pszArrayName);
+    }else{
+        printf("Array[");
+    }
+
+    for(int i = 0; i < n; ++i)
+        printf("%d,", A[i]);
+    printf("]\n");
 }
 
-void* mallocNdIntArray(const int d, const int sizes[]){
+std::vector<int> generateRandomIntVector(int size,
+                                         int lower_bound,
+                                         int upper_bound){
+    if(size < 0 )
+        return std::vector<int>();
+
+    uint32_t seed = time(NULL);
+    srand(seed);
+
+    double field = upper_bound - lower_bound;
+    std::vector<int> v;
+    for(int i = 0; i < size; ++i)
+        v.push_back(field*rand()/RAND_MAX + lower_bound);
+
+    return v;
+}
+
+void* mallocNdIntArray(const int d, const int sizes[], char initVaule = 0){
     void* p = NULL;
     if(0 >= d)
         return p;
 
-    if(1 == d)
+    if(1 == d){
         p = (void*)malloc(sizes[0]*sizeof(int));//sizeof(type)?
+        memset(p, initVaule, sizes[0]*sizeof(int));
+    }
     else{
         int** array = (int**)malloc(sizes[0]*sizeof(int*));
         for(int i = 0; i < sizes[0]; ++i) {
@@ -83,7 +112,24 @@ void* mallocNdIntArray(const int d, const int sizes[]){
     return p;
 }
 
+double gGlobalTimeStamp;
+double currentTiming()
+{
+    timeval endtime;
+    gettimeofday(&endtime,0);
+    return 1000000*(endtime.tv_sec) + endtime.tv_usec;
+    //return clock() / CLK_TCK; //start the timer
+}
+
+double currentTimingMs()
+{
+    return currentTiming()/1000;
+}
+
+
 using std::string;
 using std::vector;
+using std::set;
+using std::pair;
 
 #endif
--- sum3.cpp
+++ sum3.cpp
@@ -9,48 +9,154 @@
 
 class Solution {
 public:
-    vector<vector<int> > threeSum(vector<int> &num) {
-        if (num.empty())
-            return vector<vector<int> >();
+	//brute-force
+	vector<vector<int> > threeSum(vector<int> &num) {
+		if(num.empty())
+			return vector<vector<int> >();
 
-        int n = num.size();
-        //vector<vector<vector<int> > > vMap(3*n);
-        int sizes[] = {n, n, n};
-        int*** vMap = (int***)mallocNdIntArray(3, sizes);
-        vector<vector<int> > vResult;
-
+		int n = num.size();
+		set<vector<int> > vResult;
         for(int i = 0; i < n; ++i)
-            vMap[i][i][i] = num[i]*3;
-
-        for(int k = 0; k < n; ++k)
-            for(int i = 0; i < k; ++i)
-                for(int j = 0; j < i; ++j){
-                    vMap[i][j][k] = (vMap[i][i][i] + vMap[j][j][j] + vMap[k][k][k])/3;
-                    if(vMap[i][j][k] == 0){
-//                        std::cout << i << "," << j  << "," << k << std::endl;
-//                        std::cout << num[i] << "," << num[j]  << "," << num[k] << std::endl;
-//                        std::cout << vMap[i][i][i] << "," << vMap[j][j][j] << "," << vMap[k][k][k] << std::endl;
+			for(int j = 0; j < i; ++j)
+				for(int k = 0; k < j; ++k){
+					int sum = num[i] + num[j] + num[k];
+					if(!sum && (i != j && j != k && i != k)){
                         vector<int> vSolution;
                         vSolution.push_back(num[i]);
                         vSolution.push_back(num[j]);
                         vSolution.push_back(num[k]);
                         std::sort(vSolution.begin(), vSolution.end());
-                        vResult.push_back(vSolution);
-                        getchar();
-                    }
-                }
+                        vResult.insert(vSolution);
+					}
+				}
 
-        //std::unique(vResult.begin(), vResult.end());
-        return vResult;
+		std::vector<std::vector<int> > v(vResult.begin(), vResult.end());
+		return v;
+	}
+
+	//map based
+    vector<vector<int> > threeSum1(vector<int> &num) {
+		if(num.empty())
+			return vector<vector<int> >();
+		int n = num.size();
+		set<vector<int> > vResult;
+
+		std::map<int, std::set<std::pair<int, int> > > vVisited;
+        for(int i = 0; i < n; ++i){
+			for(int j = i + 1; j < n; ++j){
+				vVisited[num[i]+num[j]].insert(std::pair<int, int>(i, j));
+			}
+		}
+
+		for(int i = 0; i < n; ++i){
+			if(vVisited.find(-num[i]) != vVisited.end()){
+				std::set<std::pair<int, int> >::iterator it;
+				for(it = vVisited[-num[i]].begin();
+					it != vVisited[-num[i]].end();
+					++it){
+					if(i != (*it).first && i != (*it).second){
+                        vector<int> vSolution;
+                        vSolution.push_back(num[i]);
+                        vSolution.push_back(num[(*it).first]);
+						vSolution.push_back(num[(*it).second]);
+                        std::sort(vSolution.begin(), vSolution.end());
+                        vResult.insert(vSolution);
+					}
+				}
+			}
+		}
+		std::vector<std::vector<int> > v(vResult.begin(), vResult.end());
+		return v;
     }
+
+	//hash_map based
+	vector<vector<int> > threeSum2(vector<int> &num) {
+		if(num.empty())
+			return vector<vector<int> >();
+
+		int n = num.size();
+		set<vector<int> > vResult;
+
+		hash_map<int, std::set<std::pair<int, int> > > vVisited;
+        for(int i = 0; i < n; ++i){
+			for(int j = i + 1; j < n; ++j){
+				vVisited[num[i]+num[j]].insert(std::pair<int, int>(i, j));
+			}
+		}
+
+		for(int i = 0; i < n; ++i){
+			if(vVisited.find(-num[i]) != vVisited.end()){
+				std::set<std::pair<int, int> >::iterator it;
+				for(it = vVisited[-num[i]].begin();
+					it != vVisited[-num[i]].end();
+					++it){
+					if(i != (*it).first && i != (*it).second){
+                        vector<int> vSolution;
+                        vSolution.push_back(num[i]);
+                        vSolution.push_back(num[(*it).first]);
+						vSolution.push_back(num[(*it).second]);
+                        std::sort(vSolution.begin(), vSolution.end());
+                        vResult.insert(vSolution);
+					}
+				}
+			}
+		}
+		std::vector<std::vector<int> > v(vResult.begin(), vResult.end());
+		return v;
+    }
+
+	vector<vector<int> > threeSum3(vector<int> &num) {
+		if(num.empty())
+			return vector<vector<int> >();
+
+		int n = num.size();
+		set<vector<int> > vResult;
+
+		hash_map<int, int> hashNums;
+		for(int i = 0; i < n; ++i) {
+			if (hashNums.find(num[i]) == hashNums.end())
+				hashNums[num[i]] = 1;
+			else
+				hashNums[num[i]]++;
+		}
+
+        for(int i = 0; i < n; ++i){
+			hashNums[num[i]]--;
+			hash_map<int, int> savedMap = hashNums;
+			for(int j = i + 1; j < n; ++j){
+				hashNums[num[j]] --;
+				int candidate = -(num[i]+num[j]);
+				if(hashNums.find(candidate) != hashNums.end() &&
+				   hashNums[candidate]){
+					vector<int> vSolution;
+					vSolution.push_back(num[i]);
+					vSolution.push_back(num[j]);
+					vSolution.push_back(candidate);
+					std::sort(vSolution.begin(), vSolution.end());
+					vResult.insert(vSolution);
+					hashNums[candidate]--;
+				}
+				//hashNums[num[j]] ++;
+			}
+			//hashNums[num[i]]++;
+			hashNums = savedMap;
+		}
+
+
+		std::vector<std::vector<int> > v(vResult.begin(), vResult.end());
+		return v;
+	}
+
 };
 
 
 int main(int argc, char** argv)
 {
     Solution s;
-    int A[] = {-1, 0, 1, 2, -1, -4};
+    int A[] = {-6,-8,-9,4,-14,6,-10,7,12,13,4,9,7,14,-12,7,0,14,-1,-3,2,2,-3,11,-6,-10,-13,-13,1,-9,2,2,-2,8,-9,0,-9,-12,14,10,8,3,4,0,-6,7,14,9,6,-2,13,-15,8,-5,3,-13,-8,5,-11,0,11,6,-13,-14,-9,-15,-7,-11,10,-7,14,4,3,3,11,13,-13,11,-1,0,-6,-10,0,9,0,10,11,0,0,-14,-15,-12,-1,10,12,-2,2,-10,2,-2,-10,2,-13,1,12,5,-1,-15,1,5,-8,3,10,8};//{-1, 0, 1, 2, -1, -4};
     vector<int> vInput(A, A + sizeof(A)/sizeof(int));
+
+#if 0
     vector<vector<int> > v = s.threeSum(vInput);
 
     for(int i = 0; i < v.size(); ++i){
@@ -60,7 +166,65 @@ int main(int argc, char** argv)
         }
         std::cout << std::endl;
     }
+#endif
+	vector<vector<vector<int> > > vTestResults;
 
+	std::vector<int> vTest = generateRandomIntVector(1000, -10000, 10000);
+	double ts = currentTimingMs();
+	vTestResults.push_back(s.threeSum(vTest));
+	std::cout << currentTimingMs() - ts << "ms" << std::endl;
+
+	ts = currentTimingMs();
+	vTestResults.push_back(s.threeSum1(vTest));
+	std::cout << currentTimingMs() - ts << "ms" << std::endl;
+
+	ts = currentTimingMs();
+	vTestResults.push_back(s.threeSum2(vTest));
+	std::cout << currentTimingMs() - ts << "ms" << std::endl;
+
+	ts = currentTimingMs();
+	vTestResults.push_back(s.threeSum3(vTest));
+	std::cout << currentTimingMs() - ts << "ms" << std::endl;
+
+	for(int i = 0; i < vTestResults.size() - 1; ++i)
+		if ((!vTestResults[i].empty() || !vTestResults[i+1].empty()) &&
+			vTestResults[i] != vTestResults[i+1]) {
+			std::cout << "ERROR! "
+					  << "v" << i
+					  << "!="
+					  << "v" << i+1
+					  << std::endl;
+
+			std::cout << "-------------------- Input[] --------------------" << std::endl;
+			{
+				vector<int>::iterator it = vTest.begin();
+				while(it != vTest.end()){
+					std::cout << *it++ << ",";
+				}
+				std::cout << std::endl;
+			}
+
+			std::cout << "-------------------- v" << i << "--------------------" << std::endl;
+			for(int j = 0; j < vTestResults[i].size(); ++j){
+				vector<int>::iterator it = vTestResults[i][j].begin();
+				while(it != vTestResults[i][j].end()){
+					std::cout << *it++ << ",";
+				}
+				std::cout << std::endl;
+			}
+
+			std::cout << "-------------------- but v" << i + 1 << "--------------------" << std::endl;
+			for(int j = 0; j < vTestResults[i+1].size(); ++j){
+				vector<int>::iterator it = vTestResults[i+1][j].begin();
+				while(it != vTestResults[i+1][j].end()){
+					std::cout << *it++ << ",";
+				}
+				std::cout << std::endl;
+			}
+			std::cout << "----------------- end -------------------" << std::endl;
+			getchar();
+		}
+
+	std::cout << "Found " << vTestResults[0].size() << std::endl;
     return 0;
 }
-

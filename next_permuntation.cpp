d3ca4aabd926a8d790f025ea3d2f7cdc29b807bc
Wayne Wang <wythern@gmail.com>
2015-01-11 21:12:58 +0800

    make next_permuntation AC.
    Signed-off-by: Wayne Wang <wythern@gmail.com>

--- next_permuntation.cpp
+++ next_permuntation.cpp
@@ -3,10 +3,38 @@
 class Solution {
 public:
     void nextPermutation(vector<int> &num) {
+        if (num.empty())
+            return;
+
+        vector<int>::reverse_iterator r = num.rbegin();
+        for(vector<int>::reverse_iterator rIt = num.rbegin() + 1;
+            rIt != num.rend();
+            ++rIt){
+            if( *rIt >= *r){
+                r = rIt;
+            }else{
+                vector<int>::iterator it = num.end() - distance(num.rbegin(), rIt) - 1;
+                sort(it + 1, num.end());
+                swap(*upper_bound(it + 1, num.end(), *it), *it);
+                return;
+            }
+        }
+
+        sort(num.begin(), num.end());
+        return;
+    }
+
+    void nextPermutation_allrange(vector<int> &num) {
 		vector<vector<int> > vAllRange(allRange(num));
 		sort(vAllRange.begin(), vAllRange.end());
 		vector<vector<int> >::iterator it = std::upper_bound(vAllRange.begin(), vAllRange.end(), num);
-		
+#if 0
+        for(vector<vector<int> >::iterator it = vAllRange.begin();
+            it != vAllRange.end();
+            ++it){
+            printContainer<vector<int> >(*it, it->size(), "AllRange");
+        }
+#endif		
 		if(vAllRange.end() == it)
 			num = *vAllRange.begin();
 		else
@@ -35,7 +63,7 @@ public:
 		allRangeImpl(vDigits, pos+1, end, vAllRange);
 		for(vector<int>::iterator it = pos + 1; it != end; ++it){
 			swap(*it, *pos);
-			allRangeImpl(vDigits, it, end, vAllRange);
+			allRangeImpl(vDigits, pos + 1, end, vAllRange);
 			swap(*pos, *it);
 		}
 	}
@@ -52,10 +80,20 @@ int main(int argc, char** argv){
 	vector<int> num;
 	for(int i = 0; i < strPattern.size(); ++i){
 		char ch = strPattern[i];
-		num.push_back(atoi(&ch));
+		num.push_back(ch - '0');
 	}
+	printContainer<vector<int> >(num, num.size(), "Input");
+
+	vector<int> ref(num);
+	s.nextPermutation_allrange(ref);
 
 	s.nextPermutation(num);
-	printContainer<vector<int> >(num, num.size(), "Result");
+	//printContainer<vector<int> >(num, num.size(), "Result");
+    if(num != ref){
+        printContainer<vector<int> >(ref, num.size(), "Result_REF");
+        printContainer<vector<int> >(num, num.size(), "Result_NUM");
+    }else
+        printContainer<vector<int> >(num, num.size(), "Result_OUTPUT");
+
 	return 0;
 }

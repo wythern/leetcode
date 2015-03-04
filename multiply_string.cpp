1f356fb0bc14dec4f788250e3e9ea34d18887ce6
Wayne Wang <wythern@gmail.com>
2015-03-04 23:54:56 +0800

    Update multiply_string.cpp
    Refine code structure to handle decimal numbers. Not finish.
    
    Signed-off-by: Wayne Wang <wythern@gmail.com>

--- multiply_string.cpp
+++ multiply_string.cpp
@@ -3,14 +3,38 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        string r = multiplyImpl(num1, num2);
+        int num1_dot = 0;
+        int num2_dot = 0;
+        string pureNum1 = preProcessString(num1, &num1_dot);
+        string pureNum2 = preProcessString(num2, &num2_dot);
+
+        string r = multiplyImpl(pureNum1, pureNum2);
+        int dotPos = 0;
+        if(num1_dot || num2_dot){
+            dotPos = std::distance(r.rbegin() + num1_dot + num2_dot, r.rend());
+            r.insert(dotPos, 1, '.');
+        }
+
         int front = r.find_first_not_of('0');
         int end = r.size();
-        if(r.find('.') != string::npos)
-            end = std::max<int>(r.find_last_not_of('0'), r.find('.') - 1);
+        if(dotPos != 0){
+            front = std::min<int>(front, dotPos - 1);
+            end = std::max<int>(r.find_last_not_of('0'), dotPos - 1);
+        }
+
         return string(r.begin() + front, r.begin() + end);
     }
 
+    string preProcessString(string& strNum, int* dotPos){
+        int pos = strNum.find('.');
+        dotPos = 0;
+        if(pos != string::npos){
+            *dotPos = strNum.size() - pos;
+            strNum.erase(pos);
+        }
+        return strNum;
+    }
+    
     string multiplyImpl(string num1, string num2) {
         if (num1.empty() || num2.empty()){
             return string();
@@ -29,20 +53,13 @@ public:
             for(string::reverse_iterator rIt = num2.rbegin();
                 rIt != num2.rend();
                 ++rIt, ++rItResultPos){
-                if(*rIt != '.'){
-                    int interNum = (*rIt - '0') * number1 + carry;
-                    int tail = interNum % 10;
-                    *rItResultPos = tail + '0';
-                    carry = interNum / 10;
-                }else{
-                    num2_dot = std::distance(rIt, num2.rbegin());
-                }
+                int interNum = (*rIt - '0') * number1 + carry;
+                int tail = interNum % 10;
+                *rItResultPos = tail + '0';
+                carry = interNum / 10;
             }
             if (carry != 0)
                 *rItResultPos = carry + '0';
-
-            if(num2_dot != 0)
-                strResult.insert(std::distance(strResult.rbegin() + num2_dot, strResult.rend()), ".");
 
             return strResult;
         }
@@ -52,30 +69,23 @@ public:
         for(string::reverse_iterator rIt = num1.rbegin();
             rIt != num1.rend();
             ++rIt, taillen++){
-            if(*rIt != '.'){
-                string intermedium = multiplyImpl(string(1, *rIt), num2);
-                int carry = 0;
-                string::reverse_iterator rItResultPos = strResult.rbegin() + taillen;
-                for(string::reverse_iterator rItInter = intermedium.rbegin();
-                    rItInter != intermedium.rend() && rItResultPos != strResult.rend();
-                    ++rItInter, ++rItResultPos){
-                    if(*rItInter != '.'){
-                        int interNum = ((*rItInter - '0') + (*rItResultPos - '0') + carry);
-                        *rItResultPos = interNum % 10 + '0';
-                        carry = interNum / 10;
-                    }else{
-                        num2_dot = std::distance(rItInter, intermedium.rbegin()) + taillen;
-                    }
+            string intermedium = multiplyImpl(string(1, *rIt), num2);
+            int carry = 0;
+            string::reverse_iterator rItResultPos = strResult.rbegin() + taillen;
+            for(string::reverse_iterator rItInter = intermedium.rbegin();
+                rItInter != intermedium.rend() && rItResultPos != strResult.rend();
+                ++rItInter, ++rItResultPos){
+                if(*rItInter != '.'){
+                    int interNum = ((*rItInter - '0') + (*rItResultPos - '0') + carry);
+                    *rItResultPos = interNum % 10 + '0';
+                    carry = interNum / 10;
+                }else{
+                    num2_dot = std::distance(rItInter, intermedium.rbegin()) + taillen;
                 }
-                if (carry != 0)
-                    *rItResultPos = carry + '0';
-            }else{
-                num1_dot = std::distance(rIt, num1.rbegin());
             }
+            if (carry != 0)
+                *rItResultPos = carry + '0';
         }
-
-        if(num1_dot || num2_dot)
-            strResult.insert(std::distance(strResult.rbegin() + num1_dot + num2_dot, strResult.rend()), ".");
 
         return strResult;
     }
